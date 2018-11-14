164e7c778f0aba39f400eef6335769ac503baf2d
Les Matheson <les.matheson@gmail.com>
2018-11-14 07:10:37 -0500

    sync

--- test_tox.py
+++ test_tox.py
@@ -1,25 +1,26 @@
 from tox_core import *
 
+
 def test_1():
     print "stub1"
     loadIndex()
 
+
 def test_2():
 
-    ix=loadIndex('./test1/a1')
-    pm=ix.matchPaths( pattern='c*' )
-    assert len(pm)==1
+    ix = loadIndex('./test1/a1')
+    pm = ix.matchPaths(pattern='c*')
+    assert len(pm) == 1
 
-    assert len( ix.matchPaths( '*1' )) == 6 
+    assert len(ix.matchPaths('*1')) == 6
+
 
 def test_3():
     pass
 
-if __name__=="__main__" :
+
+if __name__ == "__main__":
 
     test_1()
     test_3()
     test_2()
-
-
-
