87f52926b538be960994564233d596e3e835f253
Les Matheson <les.matheson@gmail.com>
2018-11-14 07:10:00 -0500

    sync

--- tox-completion.bash
+++ tox-completion.bash
@@ -7,7 +7,7 @@
 # you set $TOXHOME=[dir] before sourcing tox-completion.bash
 
 
-export ToxPython=$(which python3 || which python)
+export ToxPython=$(which python2 || which python)
 export LmHome=${LmHome:-$HOME}
 
 export TOXHOME=${TOXHOME:-${LmHome}/bin/tox-py}
